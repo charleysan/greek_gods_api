f7e1c619cb6453d613b893a4de7466e7a6669528
charleysan <hambley1997@gmail.com>
2025-02-19 20:20:52 +1000

    Greek gods added

--- app/controllers/gods_controller.rb
+++ app/controllers/gods_controller.rb
@@ -16,7 +16,7 @@ class GodsController < ApplicationController
   end
 
   def create
-    gods = God.create(name: params[:name], power: params[:power], symbol: params[:symbol]) #dynamic
+    gods = God.create(name: params[:name], power: params[:power], symbol: params[:symbol]) #dynamic IN THE CONSOLE NEED THE KEY::VALUE PAIR EX.god = God.create(name: "Zeus",power: "Thunder",symbol: "Thunderbolt")
     render json: gods
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
  get '/gods' => 'gods#index'
  get '/gods/:id' => 'gods#show'
  post '/gods' => 'gods#create'
- put '/products/:id' => 'products#update'
- delete '/products/:id' => 'products#smite'
+ patch '/gods/:id' => 'gods#update'
+ delete '/gods/:id' => 'gods#smite'
+
 end
